4c2748a853dcabafab8c4151e0cffe5e7526fc31
stnokott <30934296+stnokott@users.noreply.github.com>
2024-12-10 10:39:32 +0100

    chore: add env var validation

--- terraform/service/variables.tf
+++ terraform/service/variables.tf
@@ -1,1 +1,8 @@
-variable "env" {}
+variable "env" {
+  type = string
+
+  validation {
+    condition     = contains(["dev", "prod"], var.env)
+    error_message = "env needs to be one of [\"dev\", \"prod\"]."
+  }
+}
