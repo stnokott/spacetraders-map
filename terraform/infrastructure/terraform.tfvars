3076808ad827d8a58ac01991b62c52914ef190e4
NoahK <30934296+stnokott@users.noreply.github.com>
2024-12-10 13:16:58 +0100

    ci: add test step, make steps conditional (#6)
    * ci: add test step, make steps conditional
    
    * chore: remove redundant TF output
    
    * fix: conditional latest-tag for image
    
    * ci: fix go test invocation

--- terraform/infrastructure/terraform.tfvars
+++ terraform/infrastructure/terraform.tfvars
@@ -1,26 +1,45 @@
 triggers = {
   tag_dev = {
     name         = "tag-dev"
-    description  = "Deploy [dev] when tag 'dev' is pushed"
+    description  = "Deploy to [dev] when tag 'dev' is pushed"
     event_type   = "push"
     filter_type  = "tag"
     filter_value = "^dev$"
-    deploy_to    = "dev"
+    steps = {
+      build = {
+        image_tag = "dev"
+      }
+      deploy = {
+        env = "dev"
+      }
+    }
   }
   pull_request_main = {
     name         = "pull-request-main"
-    description  = "Deploy [dev] when PR to 'main' is raised"
+    description  = "Deploy to [dev] when PR to 'main' is raised"
     event_type   = "pull_request"
     filter_type  = "branch"
     filter_value = "^main$"
-    deploy_to    = "dev"
+    steps = {
+      test = {}
+      build = {
+        image_tag = "$BRANCH_NAME"
+      }
+    }
   }
   push_main = {
     name         = "push-main"
-    description  = "Deploy [prod] when pushed/merged to 'main'"
+    description  = "Deploy to [prod] when pushed/merged to 'main'"
     event_type   = "push"
     filter_type  = "branch"
     filter_value = "^main$"
-    deploy_to    = "prod"
+    steps = {
+      build = {
+        image_tag = "prod"
+      }
+      deploy = {
+        env = "prod"
+      }
+    }
   }
 }
--- terraform/infrastructure/variables.tf
+++ terraform/infrastructure/variables.tf
@@ -5,25 +5,64 @@ variable "triggers" {
     event_type   = string
     filter_type  = string
     filter_value = string
-    deploy_to    = string
+    steps = object({
+      test = optional(object({}))
+      build = optional(object({
+        image_tag = string
+      }))
+      deploy = optional(object({
+        env = string
+      }))
+    })
   }))
   description = "Map of Cloud Build trigger names and their definitions. Each key will produce a separate trigger."
 
   validation {
-    condition = (
-      alltrue([for trigger in var.triggers :
-        # validate possible values for event_type
-        contains(["push", "pull_request"], trigger.event_type) &&
-        # validate possible values for filter_type
-        contains(["tag", "branch"], trigger.filter_type) &&
-        # validate filter_value not empty
-        length(trigger.filter_value) > 0 &&
-        # validate possible values for deploy_to
-        contains(["dev", "prod"], trigger.deploy_to) &&
-        # ensure we are not filtering by tag on PR (not possible)
-        !(trigger.event_type == "pull_request" && trigger.filter_type == "tag")
-      ])
-    )
-    error_message = "Trigger definitions are invalid, check variable validation."
+    condition = alltrue([for trigger in var.triggers :
+      contains(["push", "pull_request"], trigger.event_type)
+    ])
+    error_message = "Invalid value for event_type, needs to be one of ('push', 'pull_request')."
+  }
+
+  validation {
+    condition = alltrue([for trigger in var.triggers :
+      contains(["tag", "branch"], trigger.filter_type)
+    ])
+    error_message = "Invalid value for filter_type, needs to be one of ('tag', 'branch)."
+  }
+
+  validation {
+    condition = alltrue([for trigger in var.triggers :
+      length(trigger.filter_value) > 0
+    ])
+    error_message = "Invalid value for filter_value, needs to be non-empty."
+  }
+
+  validation {
+    condition = alltrue([for trigger in var.triggers :
+      !(trigger.event_type == "pull_request" && trigger.filter_type == "tag")
+    ])
+    error_message = "Cannot combine event_type = 'pull_request' and filter_type = 'tag'."
+  }
+
+  validation {
+    condition = alltrue([for trigger in var.triggers :
+      trigger.steps.deploy == null || trigger.steps.build != null
+    ])
+    error_message = "When specifying steps.deploy, steps.build also needs to be specified."
+  }
+
+  validation {
+    condition = alltrue([for trigger in var.triggers :
+      trigger.steps.deploy == null ? true : contains(["dev", "prod"], trigger.steps.deploy.env)
+    ])
+    error_message = "Invalid value for steps.deploy.env, needs to be one of ('dev', 'prod')."
+  }
+
+  validation {
+    condition = alltrue([for trigger in var.triggers :
+      trigger.steps.deploy == null ? true : trigger.steps.build.image_tag == trigger.steps.deploy.env
+    ])
+    error_message = "When specifying steps.deploy, steps.build.image_tag and steps.deploy.env need to match"
   }
 }
